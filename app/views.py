2aff5e53887594305c5cd4dcda471b8b62ab966a
unknown <harrismichael686@gmail.com>
2024-05-13 16:03:35 -0500

    implemented dynamic urls

--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ from .models import *
 # Create your views here.
 def home_view(request):
     return render(request, "home.html")
+
+
+def profile_view(request, username):
+    context = {}
+    user = User.objects.get(username=username)
+    profile = Profile.objects.get(user=user)
+    context["profile"] = profile
+    return render(request, "profile.html", context)
--- config/urls.py
+++ config/urls.py
@@ -22,4 +22,5 @@ from app.views import *
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("home/", home_view, name="home"),
+    path("<str:username>/", profile_view, name="profile"),
 ]
