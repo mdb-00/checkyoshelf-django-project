23dfbe6bb4d5a5cf449c9e99619592a7a2b965d2
unknown <harrismichael686@gmail.com>
2024-05-13 14:31:00 -0500

    added side and top nav bars

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
+
 
 # Create your views here.
+def home_view(request):
+    return render(request, "home.html")
--- config/urls.py
+++ config/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from app.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", home_view, name="home"),
 ]
